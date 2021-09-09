b0688a59d9866bc8e2ca913113ec0dc58b7804e2
Messquerade <annac.klingberg@gmail.com>
2021-09-08 21:16:52 -0700

    add HTML skeleton

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import SpaceService from './space.service';
 
-$('document').ready(function(){
-  $('End-Times').click(function() {
+$('document').ready(function () {
+  $('End-Times').click(function () {
     let promise1 = SpaceService.solarDestruction();
-    let promise2 = SpaceService
+    let promise2 = SpaceService.shockDestruction();
+    let promise3 = SpaceService.massDestruction();
+    Promise.all([promise1, promise2, promise3])
+      .then(values => {
+        
+      })
+      .catch(error => {
+        console.error(error.message)
+      });
   })
 })
